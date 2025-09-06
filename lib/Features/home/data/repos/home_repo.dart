99bbdf7556fadb3152e99e8db93936c69d80fa45
mohamedELazab111 <mobr876@gmail.com>
2025-09-06 19:24:53 +0300

    data layer done

---
+++ lib/Features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:bookly_app/Features/home/data/models/book_model/item.dart';
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<Item>>> fetchNewestBooks();
+  Future<Either<Failure, List<Item>>> fetchFeaturedBooks();
+}
---
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,45 @@
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Features/home/data/models/book_model/item.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
+  @override
+  Future<Either<Failure, List<Item>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiService.get(
+        endpoint: 'volumes?q=subject:Programming&Filtering=free-ebooks',
+      );
+      List<Item>? books = BookModel.fromJson(data).items;
+      return right(books ?? []);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<Item>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+        endpoint:
+            'volumes?q=subject:Programming&Filtering=free-ebooks&Sorting=newest',
+      );
+      List<Item>? books = BookModel.fromJson(data).items;
+      return right(books ?? []);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+  }
+}
